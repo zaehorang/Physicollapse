a64fad831606c66e894e4a97b4b0fb902de04549
zaehorang <jaho001@uos.ac.kr>
2025-02-12 16:28:16 +0900

    refactor: Update model for dynamic block size calculation
    - BlockType에 가로(widthCount) 및 세로(heightCount) 개수 설정 추가
    - Block 구조체에서 unitSize를 활용하여 동적으로 블록 크기(size) 계산
    - 블록 이미지의 크기를 10의 단위로 정렬되도록 변경

--- Physicollapse.swiftpm/Source/Block/Model/Block.swift
+++ Physicollapse.swiftpm/Source/Block/Model/Block.swift
@@ -7,9 +7,25 @@
 
 import Foundation
 
-enum BlockType {
+enum BlockType: String {
     // TODO: 더 추가 예정
-    case tBlock, oBlock
+    case tBlock, oBlock, iBlock, jBlock, lBlock, sBlock, shortIBlock, shortOBlock, shortTBlock, zBlock
+    
+    /// 블록의 가로, 세로 개수 설정 (예: oBlock은 2x2 크기)
+    var dimensions: (widthCount: Int, heightCount: Int) {
+        switch self {
+        case .tBlock: return (3, 3)
+        case .oBlock: return (2, 2)
+        case .iBlock: return (1, 4)
+        case .jBlock: return (2, 3)
+        case .lBlock: return (2, 3)
+        case .sBlock: return (3, 3)
+        case .shortIBlock: return (3, 1)
+        case .shortOBlock: return (1, 1)
+        case .shortTBlock: return (2, 3)
+        case .zBlock: return (2, 3)
+        }
+    }
 }
 
 struct Block {
@@ -17,16 +33,18 @@ struct Block {
     let size: CGSize
     let imageName: String
     
+    /// 블록의 기본 크기 (단위 크기, 기본값 50)
+    static var unitSize: CGFloat = 50.0
+    
     init(type: BlockType) {
         self.type = type
         
-        switch type {
-        case .tBlock:
-            size = CGSize(width: 50, height: 50)
-            imageName = "shortOBlock.png"
-        case .oBlock:
-            size = CGSize(width: 100, height: 100)
-            imageName = "oBlock.png"
-        }
-    }
-}
+        let (widthCount, heightCount) = type.dimensions
+        let unit = Block.unitSize
+        
+        // 동적으로 블록 크기 설정
+        self.size = CGSize(width: CGFloat(widthCount) * unit,
+                           height: CGFloat(heightCount) * unit)
+        
+        self.imageName = type.rawValue + ".png"
+    }}
